6c19c332fa6873bf7a530ce390607d279dab4e7c
Hendori <hendori@gmail.com>
2024-12-18 18:35:53 +0100

    eigen dir gemaakt voor createloops script

--- createloopsscript/createloops.sh
+++ createloopsscript/createloops.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Geef als eerste paramater mee hoe lang je loops maximaal moeten zijn
+#Geef als tweede parameter mee welke compilersetting je wil gebruiken. Bijvoorbeeld 1, 2 of 3
+
 createForLoop() {
   echo "
 void loopWithSize$1() {
@@ -10,28 +13,32 @@ void loopWithSize$1() {
 }
   "
 }
-
+compilersetting=$2
+size=$1
 code="printf(\"inline%d\",i);"
 start="#include <stdio.h>"
 start_main="int main() {"
 end_main="return 1;"
-filename="createloops.c"
+filename="createloops-$size-$compilersetting.c"
+
 rm -f ${filename}
 touch ${filename}
 #first start the file by adding include
 echo ${start} >>${filename}
 #create all the loops with specific code
-for i in $(seq 1 100); do
+for i in $(seq 1 $size); do
   createForLoop $i ${code} >>${filename}
 done
 
 #create the main method with all the loops in them
 echo ${start_main} >>${filename}
-for i in $(seq 1 100); do
+for i in $(seq 1 $size); do
   echo "  loopWithSize$i();" >>${filename}
 done
 echo ${end_main} >>${filename}
 echo "}" >>${filename}
 
-gcc ${filename} -O3 -o createloops.out
-echo "createloops.out has been created"
+gcc ${filename} -O3 -o createloops$size-$compilersetting.out
+echo "createloops$size-$compilersetting.out has been created"
+objdump -d createloops$size-$compilersetting.out >createloops$size-$compilersetting.txt
+echo "createloops$size-$compilersetting.txt gedumpt voor verdere analyze"
