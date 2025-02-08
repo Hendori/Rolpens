423ccfdae179b459eddd10b96feb343a891c6f33
Thijs van Dijk <github@inurbanus.nl>
2025-02-08 20:46:11 +0100

    Draai dit script vanuit een source-dir van een analyzer, en hij voert die automatisch uit op de binary die je opgeeft.

--- run_ghidra/run.sh
+++ run_ghidra/run.sh
@@ -10,10 +10,31 @@ if [ ! -x "$GHIDRA_PATH/support/analyzeHeadless" ]; then
 fi
 
 THIS_DIR="$(dirname "$0")"
-echo "\$0: '$0'"
-echo "THIS_DIR: '$THIS_DIR'"
+PLUGIN_DIR="$(pwd)"
 BINARY="$1"
 shift
+
+GRADLE_VERSION=8.9
+
+if [ ! -x "$THIS_DIR/gradlew" ]; then
+	cd "$THIS_DIR"
+	gradle wrapper --gradle-version $GRADLE_VERSION || (
+		echo "Failed to create a gradle wrapper. (try 'apt-get install gradle')" >&2
+		exit 1
+	)
+	cd "$PLUGIN_DIR"
+fi
+
+if [ -f "build.gradle" -a -f "extension.properties" ]; then
+	echo "Building extension..."
+	rm -f ~/.gradle/daemon/$GRADLE_VERSION/'<REPLACE>'
+	ln -sf "$GHIDRA_PATH" ~/.gradle/daemon/$GRADLE_VERSION/'<REPLACE>'
+	"$THIS_DIR/gradlew" build || exit $?
+
+	echo "Installing extension..."
+	mkdir -p "$GHIDRA_PATH/Ghidra/Extensions/$(basename "$PLUGIN_DIR")"
+	cp -r "$PLUGIN_DIR"/* "$GHIDRA_PATH/Ghidra/Extensions/$(basename "$PLUGIN_DIR")/"
+fi
 
 if [ -z "$BINARY" ]; then
 	echo "Usage: '$0' BINARY [FUNCTION_NAME[ ...]]" >&2
@@ -23,7 +44,19 @@ fi
 
 mkdir -p /tmp/ghidra
 
-exec "$GHIDRA_PATH/support/analyzeHeadless" \
+"$GHIDRA_PATH/support/analyzeHeadless" \
 	/tmp/ghidra Project1 \
 	-import "$BINARY" -overwrite \
 	-scriptPath "$THIS_DIR" -postScript decompile_simple.py "$@"
+
+EXIT_STATUS=$?
+
+if [ -f "build.gradle" -a -f "extension.properties" ]; then
+	echo "Removing extension..."
+	for plugin_jar in build/libs/*.jar; do
+		rm -rf "$GHIDRA_PATH/Ghidra/Extensions/$(basename "$PLUGIN_DIR")"
+	done
+fi
+
+exit $EXIT_STATUS
+
