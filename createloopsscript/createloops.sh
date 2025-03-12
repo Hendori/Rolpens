24b98c0a3e8463d61f7d589ef211b5ba1356715b
Hendri <hendori@gmail.com>
2025-03-12 20:55:20 +0100

    gcc met meerdere compilersettings (1,2 en 3)

--- createloopsscript/createloops.sh
+++ createloopsscript/createloops.sh
@@ -3,12 +3,9 @@
 #Geef als tweede parameter mee welke compilersetting je wil gebruiken. Bijvoorbeeld 1, 2 of 3
 #Geef aan welke functie je wil analyseren
 
-compilersetting=${2:-3}
 size=${1:-10}
-functionname=${3}
+functionname=${2}
 filename="createloops$size.c"
-executionfilename="createloops$size-$compilersetting.out"
-logoutputfilename="createloops$size-$compilersetting.txt"
 
 if [ -z "${HIGHPEECODE_PATH}" ]; then
   echo "Please set "\$"HIGHPEECODE_PATH to the location of you highpeecode repo" >&2
@@ -19,10 +16,17 @@ if [ -z "${UNROLLING_LOOPS_PATH}" ]; then
   exit 1
 fi
 
-python3 createloops.py -s ${size}
-gcc ${filename} -O$compilersetting -o $executionfilename
-echo "createloops$size-$compilersetting.out has been created"
-echo "now going to analyze this file with functionname $functionname"
-set -x
-cd ${HIGHPEECODE_PATH} && ${UNROLLING_LOOPS_PATH}run_ghidra/run.sh ${UNROLLING_LOOPS_PATH}createloopsscript/${executionfilename} ${functionname} | tee ${UNROLLING_LOOPS_PATH}createloopsscript/${logoutputfilename}
-set +x
+#Creating a c file
+python3 createloops.py -s "${size}"
+#compiling with different compilers and compiler settings and creating analysis file
+#gcc
+for compilersetting in $(seq 1 3); do
+  logoutputfilename="createloops$size-$compilersetting.txt"
+  executiongccfilename="createloops$size-$compilersetting-gcc.out"
+  echo "filename is $filename"
+  echo "compiling $executiongccfilename"
+  cd ${UNROLLING_LOOPS_PATH}/createloopsscript
+  gcc "${filename}" -O"$compilersetting" -o "$executiongccfilename"
+  echo "analysing to $logoutputfilename"
+  cd ${HIGHPEECODE_PATH} && ${UNROLLING_LOOPS_PATH}run_ghidra/run.sh ${UNROLLING_LOOPS_PATH}createloopsscript/${executiongccfilename} ${functionname} | tee ${UNROLLING_LOOPS_PATH}createloopsscript/${logoutputfilename}
+done
