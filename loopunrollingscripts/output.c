64b1d651c5aa0dd426dcbe6193419af072edfb1d
Hendori <hendori@gmail.com>
2025-03-21 20:47:50 +0100

    level 0 sample in loop-form gebracht

---
+++ loopunrollingscripts/output.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void it_crowd() {
+  puts("Are you feeling stressed, Jen?\n");
+  for (int i = 0; i < 27; i++) {
+    puts("Are you sure?\n");
+  }
+  puts("Well, I guess you're not.\n");
+  puts("Well done, Jen.\n");
+}
+
+int some_calculation(int p) {
+  int x = 0;
+  for (int i = 0; i < 8; i++) {
+    x += x * x + p;
+  }
+  return x;
+}
+
+int main(int argc, char **argv) {
+  it_crowd();
+  printf("Some calculation returned %d.\n", some_calculation(-23));
+}
