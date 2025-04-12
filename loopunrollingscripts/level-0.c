f8346d3e2ae7f5205d6bd445497fdc5c7cd23bf0
Hendori <hendori@gmail.com>
2025-04-12 11:03:57 +0200

    gepairprogrammed

--- loopunrollingscripts/level-0.c
+++ loopunrollingscripts/level-0.c
@@ -1,52 +1,69 @@
 #include <stdio.h>
 
 void it_crowd() {
-	puts("Are you feeling stressed, Jen?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Are you sure?\n");
-	puts("Well, I guess you're not.\n");
-	puts("Well done, Jen.\n");
+  for (int j = 0; j < 4; j++) {
+    puts("Hallo");
+  }
+  int i = 3;
+  if (i == 3) {
+    puts("bladiebla");
+  }
+  int x = 0;
+  int p = 4;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  puts("Are you feeling stressed, Jen?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Are you sure?\n");
+  puts("Well, I guess you're not.\n");
+  puts("Well done, Jen.\n");
 }
 
 int some_calculation(int p) {
-	int x = 0;
-	x += x*x + p;
-	x += x*x + p;
-	x += x*x + p;
-	x += x*x + p;
-	x += x*x + p;
-	x += x*x + p;
-	x += x*x + p;
-	x += x*x + p;
-	return x;
+  int x = 0;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  x += x * x + p;
+  return x;
 }
 
 int main(int argc, char **argv) {
-	it_crowd();
-	printf("Some calculation returned %d.\n", some_calculation(-23));
+  it_crowd();
+  printf("Some calculation returned %d.\n", some_calculation(-23));
 }
--- loopunrollingscripts/tree-analyse-l-0.py
+++ loopunrollingscripts/tree-analyse-l-0.py
@@ -1,7 +1,7 @@
-import os
 from tree_sitter import Language, Parser
 from ctypes import cdll, c_void_p
 from os import fspath
+import json
 
 
 def lang_from_so(path: str, name: str) -> Language:
@@ -23,7 +23,6 @@ with open("level-0.c", "r") as f:
 
 # Parse and print the syntax tree
 tree = parser.parse(source_code)
-print(tree.root_node)  # Print the AST in S-expression format
 
 
 def get_node_text(node):
@@ -31,13 +30,75 @@ def get_node_text(node):
     return source_code[node.start_byte : node.end_byte].decode()
 
 
-for child in tree.root_node.children:
-    print(get_node_text(child))
+def get_compound_statement_node(node):
+    if node.type == "compound_statement":
+        yield node
+    for child in node.children:
+        yield from get_compound_statement_node(child)
 
-previous = ""
+
+def compare_node_shapes(left, right):
+    if left.type != right.type:
+        return False
+    if len(left.children) != len(right.children):
+        return False
+    for i, left_child in enumerate(left.children):
+        if not compare_node_shapes(left_child, right.children[i]):
+            return False
+    return True
+
+
+def compare_node_content(left, right):
+    if not compare_node_shapes(left, right):
+        return False
+    match left.type:
+        case "identifier" | "string_literal":
+            return left.text == right.text
+        case _:
+            for i, left_child in enumerate(left.children):
+                if not compare_node_content(left_child, right.children[i]):
+                    return False
+            return True
 
 
 def find_duplicates(node):
-    if node == previous:
-        print("Duplicate found {0}", node)
-    previous = node
+    children_list = list(node.children)
+    for i, startnode in enumerate(children_list):
+        # print(str(startnode))
+        # print("nieuw startnode gevonden")
+        count = 0
+        for node in children_list[i + 1 :]:
+            if compare_node_content(startnode, node):
+                # print(node)
+                # print(get_node_text(node))
+                # print("gedupliceerde regel gevonden")
+                count += 1
+            else:
+                break
+        if count > 1:
+            # print(f"heey, we hebben duplicaten aantal {count}")
+            yield (startnode, count)
+
+
+def insert_loop(node):
+    node.parent.children = node.parent.children[1:]
+
+
+class Node:
+    def __init__(self, node_type, children=[]):
+        self.type = node_type
+        self.children = children
+
+    @staticmethod
+    def from_tree_sitter(self, node):
+        return Node(node.type, [Node.from_tree_sitter(child) for child in node.children]
+
+
+for child_node in get_compound_statement_node(tree.root_node):
+    for loop in sorted(find_duplicates(child_node), key=lambda x: -x[1]):
+        # print(loop)
+        # insert_loop(loop[0])
+        print(json.dumps(loop))
+        # print(child_node)
+
+        break
