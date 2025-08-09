863f9e09728e241269785f6e837765bc843f2e12
Hendri <hendori@gmail.com>
2025-08-09 10:35:07 +0200

    nieuw voorbeeld voor floats toegevoegd

---
+++ samples/level-0float.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+double local_84[];
+int iVar1, lVar2, lVar3;
+double fVar4;
+void it_crowd() {
+
+  local_84[0] = 0.0;
+  local_84[1] = 0.0;
+  local_84[2] = 0.0;
+  local_84[iVar1] = fVar4;
+  local_84[lVar2] = 0.0;
+  local_84[lVar3] = 0.0;
+  Transform3D::translated_local(local_78);
+  Vector<Vector3>::push_back(local_54, local_4c, param_23);
+  local_84[2] = 0.0;
+  local_84[0] = 0.0;
+  local_84[1] = 0.0;
+  local_84[iVar1] = local_dc;
+  local_84[lVar2] = 0.0;
+  local_84[lVar3] = 0.0;
+  Transform3D::translated_local(local_78);
+  Vector<Vector3>::push_back(local_54, local_4c, param_23);
+  local_84[2] = 0.0;
+  local_84[0] = 0.0;
+  local_84[1] = 0.0;
+  local_84[iVar1] = fVar4;
+  local_84[lVar2] = -0.25;
+  local_84[lVar3] = -0.25;
+  Transform3D::translated_local(local_78);
+  Vector<Vector3>::push_back(local_54, local_4c, param_23);
+  local_84[2] = 0.0;
+  local_84[0] = 0.0;
+  local_84[1] = 0.0;
+  local_84[iVar1] = fVar4;
+  local_84[lVar2] = -0.25;
+  local_84[lVar3] = 0.25;
+  Transform3D::translated_local(local_78);
+  Vector<Vector3>::push_back(local_54, local_4c, param_23);
+  local_84[2] = 0.0;
+  local_84[0] = 0.0;
+  local_84[1] = 0.0;
+  local_84[iVar1] = fVar4;
+  local_84[lVar2] = -0.25;
+  local_84[lVar3] = 0.25;
+  Transform3D::translated_local(local_78);
+  Vector<Vector3>::push_back(local_54, local_4c, param_23);
+}
