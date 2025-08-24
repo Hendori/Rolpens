9e76c3a9f50c93f7c426181911b037d4371fd3fa
Hendori <hendori@gmail.com>
2025-08-24 10:04:57 +0200

    added rerolled float file

---
+++ samples/rerolledlevel-0float.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+double local_84[]int iVar1double fVar4void it_crowd() {
+   for (int i = 0; i < 3; i++) {
+      local_84[i] = 0;
+   }
+
+   local_84[iVar1] = fVar4;
+   local_84[lVar2] = 0.0;
+   local_84[lVar3] = 0.0;
+   Transform3D::translated_local(local_78);
+   Vector<Vector3>::push_back(local_54, local_4c, param_23);
+   local_84[2] = 0.0;
+   local_84[0] = 0.0;
+   local_84[1] = 0.0;
+   local_84[iVar1] = local_dc;
+   local_84[lVar2] = 0.0;
+   local_84[lVar3] = 0.0;
+   Transform3D::translated_local(local_78);
+   Vector<Vector3>::push_back(local_54, local_4c, param_23);
+   for (int i = 0; i < 3; i++) {
+      local_84[2] = 0;
+      local_84[0] = 0;
+      local_84[1] = 0;
+      local_84[iVar1] = fVar4;
+      local_84[lVar2] = 0;
+      local_84[lVar3] = 0;
+      Transform3D::translated_local(local_78);
+      Vector<Vector3>::push_back(local_54, local_4c, param_23);
+   }
+
+}
+
