31436082d06e2fa53d8e799bc3397cdb2c1b6071
Thijs van Dijk <github@inurbanus.nl>
2026-02-11 12:46:27 +0100

    Compileer de drie projecten met verschillende flags

--- scripts/get-real-code.py
+++ scripts/get-real-code.py
@@ -40,35 +40,76 @@ except FileExistsError:
     pass
 os.chdir(".cache")
 
+project_versions = {
+    "coreutils": "coreutils-9.7",
+    "godot": "godot-4.4.1-stable",
+    "openssl": "openssl-3.5.0"
+}
+
 download_as("coreutils", "coreutils-9.7.tar.gz", "https://ftp.gnu.org/gnu/coreutils/coreutils-9.7.tar.gz")
 download_as("godot", "godot-4.4.1-stable.tar.gz", "https://github.com/godotengine/godot/archive/refs/tags/4.4.1-stable.tar.gz")
 download_as("openssl", "openssl-3.5.0.tar.gz", "https://github.com/openssl/openssl/releases/download/openssl-3.5.0/openssl-3.5.0.tar.gz")
 
-os.chdir("openssl/openssl-3.5.0")
-if not os.path.exists("Makefile.in"):
-    subprocess.call(["./Configure"])
-subprocess.call(["make", "-j", "4"])
-os.chdir("../..")
+flagsets = {
+    "O3": "-Wall -O3",
+    "O2": "-Wall -O2",
+    "no-unroll": "-Wall -O3 -fno-unroll-loops"
+}
 
-os.chdir("coreutils/coreutils-9.7")
-if not os.path.exists("Makefile"):
-    subprocess.call(["./configure"])
-subprocess.call(["make", "-j", "4"])
-os.chdir("../..")
+for (name, flags) in flagsets.items():
+    pv = project_versions["openssl"]
+    subprocess.check_call(["rm", "-rf", f"openssl/{pv}-{name}"])
+    subprocess.check_call(["cp", "-r", f"openssl/{pv}", f"openssl/{pv}-{name}"])
+    os.chdir(f"openssl/{pv}-{name}")
+    if not os.path.exists("Makefile.in"):
+        subprocess.check_call(["./Configure"], env={"CC": "/usr/bin/gcc", "CFLAGS": flags, "CXXFLAGS": flags})
+    subprocess.check_call(["make", "-j", "6"])
+    os.chdir("../..")
 
-os.chdir("godot/godot-4.4.1-stable")
-subprocess.call(["scons"])
-os.chdir("../..")
+    pv = project_versions["coreutils"]
+    subprocess.check_call(["rm", "-rf", f"coreutils/{pv}-{name}"])
+    subprocess.check_call(["cp", "-r", f"coreutils/{pv}", f"coreutils/{pv}-{name}"])
+    os.chdir(f"coreutils/{pv}-{name}")
+    if not os.path.exists("Makefile"):
+        with open("configure.log","w+") as f:
+            subprocess.check_call(["./configure", f"CFLAGS={flags}", f"CPPFLAGS={flags}"], stdout=f)
+    subprocess.check_call(["make", "-j", "6"])
+    os.chdir("../..")
+
+    pv = project_versions["godot"]
+    subprocess.check_call(["rm", "-rf", f"godot/{pv}-{name}"])
+    subprocess.check_call(["cp", "-r", f"godot/{pv}", f"godot/{pv}-{name}"])
+    os.chdir(f"godot/{pv}-{name}")
+    with open("scons.log","w+") as f:
+        retries = 0
+        while True:
+            retries += 1
+            try:
+                subprocess.call(["scons", f"cflags={flags}", f"ccflags={flags}", f"cppflags={flags}"], stdout=f, stderr=f)
+            except Exception as e:
+                if retries > 20:
+                    raise e
+            if os.path.exists("bin/godot.linuxbsd.editor.x86_64"):
+                break
+            elif retries > 20:
+                raise Exception("nothing seems to happen")
+    os.chdir("../..")
 
 os.chdir("..")
 workload = []
-for project in ("openssl","coreutils","godot"):
-    findcmd = ["find", f".cache/{project}", "-type", "f", "-name", "*.o"]
-    objfiles = subprocess.run(findcmd, capture_output=True, encoding="utf-8").stdout.strip().split("\n")
+for name in flagsets.keys():
+    for project, pv in project_versions.items():
+        try:
+            os.mkdir(f"decompilations/{project}-{name}")
+        except FileExistsError:
+            pass
 
-    for objfile in objfiles:
-        outfile = f"decompilations/{project}/" + " ".join(objfile.split("/")[3:])[:-2] + ".c"
-        workload.append((objfile, outfile))
+        findcmd = ["find", f".cache/{project}/{pv}-{name}", "-type", "f", "-name", "*.o"]
+        objfiles = subprocess.run(findcmd, capture_output=True, encoding="utf-8", check=True).stdout.strip().split("\n")
+
+        for objfile in objfiles:
+            outfile = f"decompilations/{project}-{name}/" + " ".join(objfile.split("/")[3:])[:-2] + ".c"
+            workload.append((objfile, outfile))
 
 random.shuffle(workload)
 
@@ -78,7 +119,7 @@ def decompile(wu):
     if os.path.exists(outfile):
         return
     try:
-        subprocess.call(["python", "./decomp-headless.py", objfile, "-o", outfile], timeout=60)
+        subprocess.call(["python", "../scripts/decomp-headless.py", objfile, "-o", outfile], timeout=60)
     except subprocess.TimeoutExpired:
         time.sleep(5)
         with open(outfile, "a+") as f:
